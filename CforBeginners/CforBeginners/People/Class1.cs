f4aa86c3ec268c98413451e8ab32c8de35326eb5
PKraakman <pkraakman@shindigz.com>
2018-09-13 14:18:32 -0400

    Up to 29

---
+++ CforBeginners/CforBeginners/People/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CforBeginners
+{
+    public class Person
+    {
+        public string firstname;
+        public string lastname;
+
+        public void Introduce()
+        {
+            Console.WriteLine("hello {0}", firstname);
+        }
+    }
+}
---
+++ CforBeginners/CforBeginners/Program.cs
@@ -0,0 +1,23 @@
+using System;
+
+
+namespace CforBeginners
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person boss = new Person();
+            boss.firstname = "jon";
+            boss.lastname = "smith";
+
+            boss.Introduce();
+
+        }
+    }
+
+  
+
+
+
+}
