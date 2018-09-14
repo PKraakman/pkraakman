6ff19eee6dda4f6f962a761ed503f9bf24a3950f
PKraakman <pkraakman@shindigz.com>
2018-09-14 16:26:00 -0400

    up to excercise 2

--- CforBeginners/CforBeginners/Program.cs
+++ CforBeginners/CforBeginners/Program.cs
@@ -3,21 +3,66 @@ using System;
 
 namespace CforBeginners
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Person boss = new Person();
-            boss.firstname = "jon";
-            boss.lastname = "smith";
+            Console.WriteLine("Please enter a number between 1 and 10");
+            var input = Console.ReadLine();
 
-            boss.Introduce();
+            var inpNumber = Convert.ToInt32(input);
 
+            switch (inpNumber)
+            {
+                case 1: firstTask();
+                break;
+
+                case 2: secondTask();
+                break;
+
+                case 3: thirdTask();
+                break;
+
+                case 4: fourthTask();
+                break;
+
+                case 5: fifthTask();
+                break;
+            }
+        }
+
+        private static void firstTask()
+        {
+            Console.WriteLine("first task");
+        }
+        static void secondTask()
+        {
+            Console.WriteLine("second task");
+            Console.WriteLine("Enter first number");
+            var input1 = Console.ReadLine();
+            var n1 = Convert.ToInt32(input1);
+
+            Console.WriteLine("Enter second number");
+            var input2 = Console.ReadLine();
+            var n2 = Convert.ToInt32(input2);
+
+            Console.WriteLine((n1 > n2) ? n1 : n2);
+        }
+
+        static void thirdTask()
+        {
+            Console.WriteLine("third task");
+        }
+
+        static void fourthTask()
+        {
+            Console.WriteLine("fourth task");
+        }
+
+        static void fifthTask()
+        {
+            Console.WriteLine("fifth task");
         }
     }
-
-  
-
-
-
 }
