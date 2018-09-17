0930c1a798c0df737f2ac81412d2805cc19cef80
PKraakman <pkraakman@shindigz.com>
2018-09-17 16:40:32 -0400

    Monday leaving work

--- CSharpIntermediate/CSharpIntermediate/Program.cs
+++ CSharpIntermediate/CSharpIntermediate/Program.cs
@@ -1,21 +1,48 @@
 ﻿
 using System;
+using System.Collections.Generic;
 
 namespace CSharpIntermediate
 {
+    class Stack
+    {
+        private List<int> _stack;
+
+        public Stack()
+        {
+            _stack = new List<int>();
+        }
+
+        public void Push(int number)
+        {
+            _stack.Add(number);
+        }
+        public int Pop()
+        {
+            var lastitem = _stack[_stack.Count - 1];
+            _stack.RemoveAt(_stack.Count - 1);
+            return lastitem;
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var customer = new Customer();
-            customer.Id = 1;
-            customer.Name = "John";
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
 
-            var order = new Order();
-            customer.Orders.Add(order);
+            stack.Clear();
 
-            Console.WriteLine(customer.Id);
-            Console.WriteLine(customer.Name);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
         }
     }
 }
